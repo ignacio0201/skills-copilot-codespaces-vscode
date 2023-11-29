bd03bdc98e8aee0971abed974c57d9f6bd08d927
Ignacio Miranda Arévalo <ignacio.miranda.arevalo@gmail.com>
2023-11-29 06:51:10 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,16 @@
+// Create web server
+// Reference: https://codeforgeek.com/2015/07/using-async-await-in-node-js/
+// Reference: https://www.sitepoint.com/overview-javascript-promises/
+// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await
+
+// Import modules
+const express = require('express')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const path = require('path')
+
+// Constants
+const PORT = 3000
+const HOST = ''
